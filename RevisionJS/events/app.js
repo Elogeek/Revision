d9cc47f97b72dd210075666b7c325c8ca45577a7
elogeek <elodiechristin@gmail.com>
2021-09-03 16:40:19 +0200

    let's go

--- RevisionJS/events/app.js
+++ RevisionJS/events/app.js
@@ -1,2 +1,16 @@
 let btnOpen = document.getElementById('open');
 let btnClose = document.getElementById('close');
+let newWindow = '';
+
+btnOpen.addEventListener('click',wOpen);
+btnOpen.addEventListener('click', wClose);
+
+/* Open the window this click */
+function wOpen() {
+    newWindow = window.open('https://youtube.com', '_blank', 'width = 600');
+}
+
+/* Close window */
+function wClose() {
+    newWindow.close();
+}
