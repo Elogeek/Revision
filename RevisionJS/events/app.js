eab382aac2eb3697c7e7d0daf796ee596f031eaa
elogeek <elodiechristin@gmail.com>
2021-09-03 16:55:28 +0200

    let's go

--- RevisionJS/events/app.js
+++ RevisionJS/events/app.js
@@ -1,9 +1,11 @@
 let btnOpen = document.getElementById('open');
 let btnClose = document.getElementById('close');
+let btnChangeSizeW = document.getElementById('changeSizeW');
 let newWindow = '';
 
 btnOpen.addEventListener('click',wOpen);
 btnClose.addEventListener('click', wClose);
+btnChangeSizeW.addEventListener('click', changeW);
 
 /* Open the window this click */
 function wOpen() {
@@ -13,4 +15,10 @@ function wOpen() {
 /* Close window */
 function wClose() {
     newWindow.close();
+}
+
+/* Modify size window */
+function changeW() {
+    newWindow.resizeTo(250, 250);
+
 }
